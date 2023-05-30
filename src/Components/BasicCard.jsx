3fa2d11134c5968cc91002f91b501d53c1748aef
Danu Villa <126210362+DanuVillaContini@users.noreply.github.com>
2023-05-30 20:59:36 -0300

    commit imagen factura de cards menu factura

--- src/Components/BasicCard.jsx
+++ src/Components/BasicCard.jsx
@@ -1,10 +1,11 @@
 import {Card, Button, Col} from "react-bootstrap"
+import facturaImg from "../assets/facturaImg.png"
 
 function BasicCard(props) {
     return (
         <>
             <Card style={{ width: '15rem' }} >
-                <Col className="d-flex justify-content-center"><Card.Img className="w-50" variant="top" src="https://solicitalia.es/wp-content/uploads/2020/10/Como-hacer-una-factura-logo.jpg"/></Col>
+                <Col className="d-flex justify-content-center mb-2"><Card.Img className="w-50" variant="top" src={facturaImg} /></Col>
                 <Card.Body>
                     <Card.Title className="py-1">{props.title}</Card.Title>
                     <Button variant="dark" className="py-1" href={props.link}>Emitir</Button>
